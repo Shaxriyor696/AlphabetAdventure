ce27796801f5707d7d966e5e7a22688d64845bde
Shaxriyor696 <shaxriyorpirnazarov8@gmail.com>
2025-02-18 14:41:58 +0500

    Update routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
-import AlphabetGame from "@/pages";
+import AlphabetGame from "@/pages/AlphabetAdventure";
 
 function Router() {
   return (
