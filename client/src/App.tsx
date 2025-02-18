3a20bfc4f07b46099a124e9efaa3a3824f65b1fb
Shaxriyor696 <shaxriyorpirnazarov8@gmail.com>
2025-02-18 15:44:02 +0500

    patch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,8 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import NotFound from "@/pages/AlphabetAdventure/not-found";
-import AlphabetGame from "@/pages/AlphabetAdventure";
+import NotFound from "@/pages/not-found";
+import AlphabetGame from "@/pages";
 
 function Router() {
   return (
