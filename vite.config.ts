7dea3c3cbf9e9fe9eec80e424fa63a8d1bcf7d94
Shaxriyor696 <shaxriyorpirnazarov8@gmail.com>
2025-02-18 15:50:06 +0500

    patch

--- vite.config.ts
+++ vite.config.ts
@@ -10,16 +10,16 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
-  base: "/AlphabetAdventure/",
+  base: "https://shaxriyor696.github.io/AlphabetAdventure/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "client", "src"),
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: "../dist",
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
 });
